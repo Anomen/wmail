6e76260b6605171b2c7306822da8a8e9b1b299ad
cyril <cyril@nexu.org>
2010-06-19 15:05:56 +0200

    Creation de page
    	new file:   include/mainpage.h
    	new file:   source/mainpage.cxx
    	modified:   include/wmailapplication.h
    	modified:   source/CMakeLists.txt
    	modified:   source/wmail.cxx
    	modified:   source/wmailapplication.cxx

---
+++ include/mainpage.h
@@ -0,0 +1,21 @@
+#ifndef __mainpage_h
+#define __mainpage_c
+
+#include <Wt/WContainerWidget>
+
+using namespace Wt;
+
+class MainPage : public WContainerWidget
+{
+public:
+
+  MainPage();
+  
+
+
+private:
+
+
+};
+
+#endif
